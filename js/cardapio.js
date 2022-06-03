4514ff83cdd627fccef7dd0c6f790f032101a37b
eduardor0cha <c.eduardo_rn@outlook.com>
2022-06-02 23:39:07 -0300

    Add icons

--- js/cardapio.js
+++ js/cardapio.js
@@ -3,12 +3,12 @@ function filterCategories() {
   var filter = select.value.toLowerCase();
   var items = document.getElementsByClassName("product-category");
   for (var i = 0; i < items.length; i++) {
-    var itemClass = items[i].getAttribute("data-item");
+    var itemClass = items[i].dataset.item;
     if (filter == "all") {
-      items[i].style.display = "flex";
+      items[i].style.display = "initial";
     } else {
       if (itemClass.toLowerCase() == filter) {
-        items[i].style.display = "flex";
+        items[i].style.display = "initial";
       } else {
         items[i].style.display = "none";
       }
